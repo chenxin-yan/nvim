4985967e2fea558a111550dfff6e02b4a6c89b78
Chenxin Yan (Cyan) <cy2558@nyu.edu>
2024-07-04 01:38:13 -0400

    refactor cmp keymaps; setup luanip snippet loader

--- lua/chenxinyan/plugins/cmp.lua
+++ lua/chenxinyan/plugins/cmp.lua
@@ -38,7 +38,14 @@ return { -- Autocompletion
     -- See `:help cmp`
     local cmp = require 'cmp'
     local luasnip = require 'luasnip'
-    luasnip.config.setup {}
+    luasnip.config.setup {
+      update_events = { 'TextChanged', 'TextChangedI' },
+      enable_autosnippets = true,
+    }
+
+    -- NOTE: Load custom lua snippets
+    require('luasnip.loaders.from_lua').load { paths = { '~/.config/nvim/lua/chenxinyan/snippets/' } }
+    vim.keymap.set('n', '<leader>sp', '<cmd>source ~/.config/nvim/lua/chenxinyan/plugins/cmp.lua')
 
     cmp.setup {
       snippet = {
@@ -53,23 +60,20 @@ return { -- Autocompletion
       --
       -- No, but seriously. Please read `:help ins-completion`, it is really good!
       mapping = cmp.mapping.preset.insert {
-        -- Select the [n]ext item
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        -- Select the [p]revious item
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-j>'] = cmp.mapping.select_next_item(),
+        ['<C-k>'] = cmp.mapping.select_prev_item(),
 
-        -- Scroll the documentation window [b]ack / [f]orward
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
 
         -- Accept ([y]es) the completion.
         --  This will auto-import if your LSP supports it.
         --  This will expand snippets if the LSP sent a snippet.
-        ['<C-y>'] = cmp.mapping.confirm { select = true },
+        -- ['<C-y>'] = cmp.mapping.confirm { select = true },
         --
         -- If you prefer more traditional completion keymaps,
         -- you can uncomment the following lines
-        -- ['<CR>'] = cmp.mapping.confirm { select = true },
+        ['<CR>'] = cmp.mapping.confirm { select = true },
         --['<Tab>'] = cmp.mapping.select_next_item(),
         --['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
