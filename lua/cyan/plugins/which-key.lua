ffaa20581565fb6efd404e92b231640f1376b437
Chenxin Yan (Cyan) <cy2558@nyu.edu>
2024-07-13 16:43:52 -0400

    add whichkey group using opts.spec

--- lua/cyan/plugins/which-key.lua
+++ lua/cyan/plugins/which-key.lua
@@ -1,11 +1,30 @@
-return { -- Useful plugin to show you pending keybinds.
+return {
   'folke/which-key.nvim',
   event = { 'BufReadPre', 'BufNewFile' },
   opts = {
+    spec = {
+      { 'g', group = '[G]oto' },
+      { 'z', group = 'Fold' },
+      { ']', group = 'Next' },
+      { '[', group = 'Previous' },
+      { 's', group = '[S]urround' },
+      { '<leader>c', group = '󰘦 [C]ode' },
+      { '<leader>d', group = '󰈙 [D]ocument' },
+      { '<leader>s', group = ' [S]earch' },
+      { '<leader>w', group = ' [W]orkspace' },
+      { '<leader>t', group = '󰔡 [T]oggle' },
+      { '<leader>g', group = ' [G]it' },
+      { '<leader>h', group = ' [H]unk' },
+      { '<leader>b', group = ' [B]uffer' },
+      { '<leader>x', group = ' [X]Diagnostics/Quickfix' },
+    },
     plugins = {
       spelling = {
         enabled = false,
       },
+    },
+    modes = {
+      t = false,
     },
     icons = {
       rules = false,
@@ -15,24 +34,5 @@ return { -- Useful plugin to show you pending keybinds.
   config = function(_, opts)
     local wk = require 'which-key'
     wk.setup(opts)
-    wk.add {
-      {
-        { 'g', group = '[G]oto' },
-        { 'z', group = 'Fold' },
-        { ']', group = 'Next' },
-        { '[', group = 'Previous' },
-        { 's', group = '[S]urround' },
-
-        { '<leader>c', group = '󰘦 [C]ode' },
-        { '<leader>d', group = '󰈙 [D]ocument' },
-        { '<leader>s', group = ' [S]earch' },
-        { '<leader>w', group = ' [W]orkspace' },
-        { '<leader>t', group = '󰔡 [T]oggle' },
-        { '<leader>g', group = ' [G]it' },
-        { '<leader>h', group = ' [H]unk' },
-        { '<leader>b', group = ' [B]uffer' },
-        { '<leader>x', group = ' [X]Diagnostics/Quickfix' },
-      },
-    }
   end,
 }
