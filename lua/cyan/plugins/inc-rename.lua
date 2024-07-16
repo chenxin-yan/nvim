18bd24b4a94126a1a682d72fcae5647485bbeb63
Chenxin Yan (Cyan) <cy2558@nyu.edu>
2024-07-16 17:23:08 -0400

    optimize plugin lazyload

--- lua/cyan/plugins/inc-rename.lua
+++ lua/cyan/plugins/inc-rename.lua
@@ -1,10 +1,15 @@
 return { -- incremental renaming
   'smjonas/inc-rename.nvim',
-  event = { 'BufReadPre', 'BufNewFile' },
+  keys = {
+    {
+      '<leader>cr',
+      ':IncRename ',
+      desc = '[R]ename',
+    },
+  },
   config = function()
     require('inc_rename').setup {
       save_in_cmdline_history = false,
     }
-    vim.keymap.set('n', '<leader>cr', ':IncRename ', { desc = '[R]ename' })
   end,
 }
--- lua/cyan/plugins/refactoring.lua
+++ lua/cyan/plugins/refactoring.lua
@@ -1,6 +1,15 @@
 return {
   'ThePrimeagen/refactoring.nvim',
   event = { 'BufReadPre', 'BufNewFile' },
+  keys = {
+    {
+      '<leader>cR',
+      function()
+        require('telescope').extensions.refactoring.refactors()
+      end,
+      desc = '[C]ode [R]efactoring',
+    },
+  },
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-treesitter/nvim-treesitter',
@@ -10,9 +19,5 @@ return {
     require('refactoring').setup()
     -- load refactoring Telescope extension
     require('telescope').load_extension 'refactoring'
-
-    vim.keymap.set({ 'n', 'x' }, '<leader>cR', function()
-      require('telescope').extensions.refactoring.refactors()
-    end, { desc = '[C]ode [R]efactoring' })
   end,
 }
