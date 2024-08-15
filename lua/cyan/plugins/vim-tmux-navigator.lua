a23b2e1e8e92ffaa0ead706fb25d5137b546fde8
Chenxin Yan (Cyan) <cy2558@nyu.edu>
2024-08-15 03:35:46 -0400

    repace tmux navigator to navigator.nvim

--- lua/cyan/plugins/vim-tmux-navigator.lua
+++ lua/cyan/plugins/vim-tmux-navigator.lua
@@ -1,17 +1,21 @@
 return {
-  'christoomey/vim-tmux-navigator',
+  'numToStr/Navigator.nvim',
   cmd = {
-    'TmuxNavigateLeft',
-    'TmuxNavigateDown',
-    'TmuxNavigateUp',
-    'TmuxNavigateRight',
-    'TmuxNavigatePrevious',
+    'NavigatorLeft',
+    'NavigatorDown',
+    'NavigatorUp',
+    'NavigatorRight',
+    'NavigatorPrevious',
   },
   keys = {
-    { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
-    { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
-    { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
-    { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
-    { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+    { '<c-h>', '<cmd>NavigatorLeft<cr>' },
+    { '<c-j>', '<cmd>NavigatorDown<cr>' },
+    { '<c-k>', '<cmd>NavigatorUp<cr>' },
+    { '<c-l>', '<cmd>NavigatorRight<cr>' },
+    { '<c-\\>', '<cmd>NavigatorPrevious<cr>' },
+  },
+  opts = {
+    auto_save = 'all',
+    disable_on_zoom = true,
   },
 }
