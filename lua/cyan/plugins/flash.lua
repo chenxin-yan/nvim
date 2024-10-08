1f394731d5b9845e0c4862dc7e30fe7bd2ab566a
Chenxin Yan (Cyan) <cy2558@nyu.edu>
2024-10-08 00:41:37 -0400

    setup update flash.nvim config

--- lua/cyan/plugins/flash.lua
+++ lua/cyan/plugins/flash.lua
@@ -15,22 +15,36 @@ return {
     },
     {
       'S',
+      mode = { 'n', 'o', 'x' },
+      function()
+        require('flash').treesitter()
+      end,
+      desc = 'Flash Treesitter',
+    },
+    {
+      'r',
+      mode = 'o',
+      function()
+        require('flash').remote()
+      end,
+      desc = 'Remote Flash',
+    },
+    {
+      'R',
+      mode = { 'o', 'x' },
+      function()
+        require('flash').treesitter_search()
+      end,
+      desc = 'Treesitter Search',
+    },
+    {
+      '<c-s>',
       mode = { 'n', 'x', 'o' },
       function()
         require('flash').jump {
-          pattern = '.', -- initialize pattern with any char
-          search = {
-            mode = function(pattern)
-              -- remove leading dot
-              if pattern:sub(1, 1) == '.' then
-                pattern = pattern:sub(2)
-              end
-              -- return word pattern and proper skip pattern
-              return ([[\<%s\w*\>]]):format(pattern), ([[\<%s]]):format(pattern)
-            end,
-          },
-          -- select the range
-          jump = { pos = 'range' },
+          search = { mode = 'search', max_length = 0 },
+          label = { after = { 0, 0 } },
+          pattern = '^',
         }
       end,
       desc = 'Flash to a line',
--- lua/cyan/plugins/telescope.lua
+++ lua/cyan/plugins/telescope.lua
@@ -101,16 +101,38 @@ return { -- Fuzzy Finder (files, lsp, etc)
     -- See `:help telescope` and `:help telescope.setup()`
     local actions = require 'telescope.actions'
 
+    -- flash integrations
+    local function flash(prompt_bufnr)
+      require('flash').jump {
+        pattern = '^',
+        label = { after = { 0, 0 } },
+        search = {
+          mode = 'search',
+          exclude = {
+            function(win)
+              return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= 'TelescopeResults'
+            end,
+          },
+        },
+        action = function(match)
+          local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+          picker:set_selection(match.pos[1] - 1)
+        end,
+      }
+    end
+
     require('telescope').setup {
       -- You can put your default mappings / updates / etc. in here
       --  All the info you're looking for is in `:help telescope.setup()`
       defaults = {
         mappings = {
+          n = { s = flash },
           i = {
             ['<C-j>'] = actions.move_selection_next,
             ['<C-k>'] = actions.move_selection_previous,
             ['<C-n>'] = actions.cycle_history_next,
             ['<C-p>'] = actions.cycle_history_prev,
+            ['<c-s>'] = flash,
           },
         },
       },
