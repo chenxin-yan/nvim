b048e49be6c225962bf0ddca4fa6bf446a82ecba
Chenxin Yan (Cyan) <cxyan04@gmail.com>
2024-11-01 01:58:57 -0400

    remove quit terminal keymap

--- lua/cyan/plugins/FTerm.lua
+++ lua/cyan/plugins/FTerm.lua
@@ -3,7 +3,6 @@ return {
   keys = {
     { '<C-\\>', '<cmd>lua require("FTerm").toggle()<cr>', desc = 'Toggle [T]erminal', mode = { 'n' } },
     { '<C-\\>', '<C-\\><C-n><cmd>lua require("FTerm").toggle()<cr>', desc = 'Toggle [T]erminal', mode = { 't' } },
-    { '<C-q>', '<C-\\><C-n><cmd>lua require("FTerm").exit()<cr>', desc = 'Exit [T]erminal', mode = { 't' } },
   },
   config = true,
 }
