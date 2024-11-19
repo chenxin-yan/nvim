11b160b1cbc4cffca7181791f39be94995cc65af
Chenxin Yan (Cyan) <cxyan04@gmail.com>
2024-11-19 00:25:03 -0500

    setup image pasting support for avante.nvim

--- lua/cyan/plugins/avante.lua
+++ lua/cyan/plugins/avante.lua
@@ -21,5 +21,22 @@ return {
     'nvim-lua/plenary.nvim',
     'MunifTanjim/nui.nvim',
     'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+    {
+      -- support for image pasting
+      'HakonHarnes/img-clip.nvim',
+      event = 'VeryLazy',
+      opts = {
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+        },
+      },
+      keys = {
+        { '<leader>P', '<cmd>PasteImage<cr>', desc = 'Paste image from system clipboard' },
+      },
+    },
   },
 }
