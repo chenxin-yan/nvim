0f35bcb50f21e9165d6831bf17854ccdbf01e6e2
Chenxin Yan (Cyan) <cxyan04@gmail.com>
2024-12-10 16:01:48 -0500

    edit zen config

--- lua/cyan/plugins/zen.lua
+++ lua/cyan/plugins/zen.lua
@@ -5,17 +5,6 @@ return {
     { '<leader>tz', '<cmd>ZenMode<cr>', desc = '[T]oggle [Z]en mode' },
   },
   opts = {
-    window = {
-      options = {
-        -- signcolumn = "no", -- disable signcolumn
-        number = false, -- disable number column
-        relativenumber = false, -- disable relative numbers
-        -- cursorline = false, -- disable cursorline
-        -- cursorcolumn = false, -- disable cursor column
-        -- foldcolumn = "0", -- disable fold column
-        -- list = false, -- disable whitespace characters
-      },
-    },
     plugins = {
       gitsigns = { enabled = true },
     },
