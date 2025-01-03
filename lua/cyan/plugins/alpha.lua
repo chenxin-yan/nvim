52e8cd28eea25edbb55a71e7bdf55d307212f914
Chenxin Yan (Cyan) <cxyan04@gmail.com>
2025-01-02 20:42:18 -0500

    disable statusline in alpha dashboard

--- lua/cyan/plugins/alpha.lua
+++ lua/cyan/plugins/alpha.lua
@@ -96,6 +96,17 @@ return {
 
     require('alpha').setup(dashboard.opts)
 
+    -- Set laststatus=0 when in dashboard
+    vim.o.laststatus = 0
+
+    -- Reset laststatus when leaving dashboard
+    vim.api.nvim_create_autocmd('BufUnload', {
+      buffer = 0,
+      callback = function()
+        vim.o.laststatus = 3
+      end,
+    })
+
     vim.api.nvim_create_autocmd('User', {
       once = true,
       pattern = 'LazyVimStarted',
