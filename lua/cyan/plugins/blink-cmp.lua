a66293fa7ccc488f15bc5ed91d58ca4b44827088
Chenxin Yan (Cyan) <cxyan04@gmail.com>
2025-01-19 18:41:36 -0500

    update blink-cmp.lua

--- lua/cyan/plugins/blink-cmp.lua
+++ lua/cyan/plugins/blink-cmp.lua
@@ -168,18 +168,6 @@ return {
       },
       snippets = {
         preset = 'luasnip',
-        expand = function(snippet)
-          require('luasnip').lsp_expand(snippet)
-        end,
-        active = function(filter)
-          if filter and filter.direction then
-            return require('luasnip').jumpable(filter.direction)
-          end
-          return require('luasnip').in_snippet()
-        end,
-        jump = function(direction)
-          require('luasnip').jump(direction)
-        end,
       },
       sources = {
         default = {
