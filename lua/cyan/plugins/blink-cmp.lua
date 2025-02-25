8ccb88e13dc0685916c6d0738e3b27ac9d77256a
chenxin-yan <cxyan04@gmail.com>
2025-02-25 02:53:28 -0500

    add blink git customizations

--- lua/cyan/plugins/blink-cmp.lua
+++ lua/cyan/plugins/blink-cmp.lua
@@ -2,6 +2,66 @@ if vim.g.vscode then
   return {}
 end
 
+-- blink git customizations
+local blink_cmp_kind_name_highlight = {
+  Commit = { default = false, fg = '#a6e3a1' },
+  Mention = { default = false, fg = '#a6e3a1' },
+  openPR = { default = false, fg = '#a6e3a1' },
+  openedPR = { default = false, fg = '#a6e3a1' },
+  closedPR = { default = false, fg = '#f38ba8' },
+  mergedPR = { default = false, fg = '#cba6f7' },
+  draftPR = { default = false, fg = '#9399b2' },
+  lockedPR = { default = false, fg = '#f5c2e7' },
+  openIssue = { default = false, fg = '#a6e3a1' },
+  openedIssue = { default = false, fg = '#a6e3a1' },
+  reopenedIssue = { default = false, fg = '#a6e3a1' },
+  completedIssue = { default = false, fg = '#cba6f7' },
+  closedIssue = { default = false, fg = '#cba6f7' },
+  not_plannedIssue = { default = false, fg = '#9399b2' },
+  duplicateIssue = { default = false, fg = '#9399b2' },
+  lockedIssue = { default = false, fg = '#f5c2e7' },
+}
+for kind_name, hl in pairs(blink_cmp_kind_name_highlight) do
+  vim.api.nvim_set_hl(0, 'BlinkCmpGitKind' .. kind_name, hl)
+  vim.api.nvim_set_hl(0, 'BlinkCmpGitKindIcon' .. kind_name, hl)
+  vim.api.nvim_set_hl(0, 'BlinkCmpGitLabel' .. kind_name .. 'Id', hl)
+end
+
+local function pr_or_issue_configure_score_offset(items)
+  -- Bonus to make sure items sorted as below:
+  local keys = {
+    -- place `kind_name` here
+    { 'openIssue', 'openedIssue', 'reopenedIssue' },
+    { 'openPR', 'openedPR' },
+    { 'lockedIssue', 'lockedPR' },
+    { 'completedIssue' },
+    { 'draftPR' },
+    { 'mergedPR' },
+    { 'closedPR', 'closedIssue', 'not_plannedIssue', 'duplicateIssue' },
+  }
+  local bonus = 999999
+  local bonus_score = {}
+  for i = 1, #keys do
+    for _, key in ipairs(keys[i]) do
+      bonus_score[key] = bonus * (#keys - i)
+    end
+  end
+  for i = 1, #items do
+    local bonus_key = items[i].kind_name
+    if bonus_score[bonus_key] then
+      items[i].score_offset = bonus_score[bonus_key]
+    end
+    -- sort by number when having the same bonus score
+    local number = items[i].label:match '[#!](%d+)'
+    if number then
+      if items[i].score_offset == nil then
+        items[i].score_offset = 0
+      end
+      items[i].score_offset = items[i].score_offset + tonumber(number)
+    end
+  end
+end
+
 return {
   -- {
   --   'saghen/blink.compat',
@@ -159,6 +219,21 @@ return {
           Event = '󱐋',
           Operator = '󰪚',
           TypeParameter = '󰬛',
+
+          openPR = '',
+          openedPR = '',
+          closedPR = '',
+          mergedPR = '',
+          draftPR = '',
+          lockedPR = '',
+          openIssue = '',
+          openedIssue = '',
+          reopenedIssue = '',
+          completedIssue = '',
+          closedIssue = '',
+          not_plannedIssue = '',
+          duplicateIssue = '',
+          lockedIssue = '',
         },
       },
       completion = {
@@ -243,6 +318,47 @@ return {
             enabled = function()
               return vim.tbl_contains({ 'octo', 'gitcommit', 'markdown' }, vim.bo.filetype)
             end,
+            opts = {
+              git_centers = {
+                github = {
+                  pull_request = {
+                    get_command_args = function(command, token)
+                      local args = require('blink-cmp-git.default.github').issue.get_command_args(command, token)
+                      local utils = require 'blink-cmp-git.utils'
+                      if command == 'curl' then
+                        args[#args] = 'https://api.github.com/repos/' .. utils.get_repo_owner_and_repo() .. '/pulls?state=all&per_page=100&sort=updated'
+                      else
+                        args[#args] = 'repos/' .. utils.get_repo_owner_and_repo() .. '/pulls?state=all&per_page=100&sort=updated'
+                      end
+                      return args
+                    end,
+                    get_kind_name = function(item)
+                      -- openPR, closedPR, mergedPR, draftPR, lockedPR
+                      return item.locked and 'lockedPR' or item.draft and 'draftPR' or item.merged_at and 'mergedPR' or item.state .. 'PR'
+                    end,
+                    configure_score_offset = pr_or_issue_configure_score_offset,
+                  },
+                  issue = {
+                    get_command_args = function(command, token)
+                      local args = require('blink-cmp-git.default.github').issue.get_command_args(command, token)
+                      local utils = require 'blink-cmp-git.utils'
+                      if command == 'curl' then
+                        args[#args] = 'https://api.github.com/repos/' .. utils.get_repo_owner_and_repo() .. '/issues?state=all&per_page=100&sort=updated'
+                      else
+                        args[#args] = 'repos/' .. utils.get_repo_owner_and_repo() .. '/issues?state=all&per_page=100&sort=updated'
+                      end
+                      return args
+                    end,
+                    get_kind_name = function(item)
+                      -- openIssue, reopenedIssue, completedIssue
+                      -- not_plannedIssue, lockedIssue, duplicateIssue
+                      return item.locked and 'lockedIssue' or (item.state_reason or item.state) .. 'Issue'
+                    end,
+                    configure_score_offset = pr_or_issue_configure_score_offset,
+                  },
+                },
+              },
+            },
           },
           avante = {
             module = 'blink-cmp-avante',
